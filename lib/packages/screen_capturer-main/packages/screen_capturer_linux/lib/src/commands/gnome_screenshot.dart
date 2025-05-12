94da4109668169e741519ddb269af8d1a022e66a
sergey <gentofluck03@gmail.com>
2025-05-12 22:36:23 +0000

    flutter bug fix

--- lib/packages/screen_capturer-main/packages/screen_capturer_linux/lib/src/commands/gnome_screenshot.dart
+++ lib/packages/screen_capturer-main/packages/screen_capturer_linux/lib/src/commands/gnome_screenshot.dart
@@ -1,13 +1,38 @@
+import 'dart:io';
 import 'dart:typed_data';
+import 'package:flutter/services.dart';
 import 'package:screen_capturer_platform_interface/screen_capturer_platform_interface.dart';
 import 'package:shell_executor/shell_executor.dart';
-import 'package:flutter/services.dart';
 
 class GnomeScreenshot implements SystemScreenCapturer {
   @override
   String get executable => 'gnome-screenshot';
 
   static const _platform = MethodChannel('dev.leanflutter.plugins/screen_capturer');
+
+  Future<bool> _isCommandAvailable(String command) async {
+    final result = await ShellExecutor.global.exec('which', [command]);
+    return result.exitCode == 0;
+  }
+
+  Future<void> _copyImageToClipboard(String imagePath) async {
+    final isWlCopyAvailable = await _isCommandAvailable('wl-copy');
+    final isXclipAvailable = await _isCommandAvailable('xclip');
+
+    if (isWlCopyAvailable) {
+      await ShellExecutor.global.exec('bash', [
+        '-c',
+        'wl-copy < "$imagePath"'
+      ]);
+    } else if (isXclipAvailable) {
+      await ShellExecutor.global.exec('bash', [
+        '-c',
+        'xclip -selection clipboard -t image/png -i "$imagePath"'
+      ]);
+    } else {
+      throw Exception('Neither wl-copy nor xclip is available');
+    }
+  }
 
   @override
   Future<Uint8List> capture({
@@ -16,20 +41,21 @@ class GnomeScreenshot implements SystemScreenCapturer {
     bool copyToClipboard = true,
     bool silent = true,
   }) async {
-    // Генерируем уникальное имя временного файла
     final tempFile = '/tmp/shot_${DateTime.now().millisecondsSinceEpoch}.png';
-    
+
     try {
-      // Выполняем команду для создания скриншота и копирования в буфер
       await ShellExecutor.global.exec(
         'bash',
         [
           '-c',
-          'gnome-screenshot -a -f "$tempFile" && wl-copy < "$tempFile" && rm "$tempFile"'
+          'gnome-screenshot -a -f "$tempFile"'
         ],
       );
 
-      // Получаем изображение из буфера обмена
+      if (copyToClipboard) {
+        await _copyImageToClipboard(tempFile);
+      }
+
       final Uint8List? image = await _platform.invokeMethod<Uint8List>('readImageFromClipboard');
 
       return Uint8List.fromList(image ?? []);
@@ -40,5 +66,4 @@ class GnomeScreenshot implements SystemScreenCapturer {
   }
 }
 
-// Создаем экземпляр GnomeScreenshot
 final gnomeScreenshot = GnomeScreenshot();
